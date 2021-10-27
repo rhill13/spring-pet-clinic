0beb3f095a1339611fe391530608692d2d8d8ce9
Ryan <ryanhill1128@gmail.com>
2021-10-26 23:29:05 -0500

    refactoring to multimodule maven project. closes #1.

---
+++ pet-clinic-data/src/test/java/biz/rookware/AppTest.java
@@ -0,0 +1,20 @@
+package biz.rookware;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
