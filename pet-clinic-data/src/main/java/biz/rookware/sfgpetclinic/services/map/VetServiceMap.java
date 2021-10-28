b6ca1446c0c51d6851bba24b51504e8996c4b131
Ryan <ryanhill1128@gmail.com>
2021-10-28 11:06:34 -0500

    impl map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package biz.rookware.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import biz.rookware.sfgpetclinic.model.Vet;
+import biz.rookware.sfgpetclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+    
+}
