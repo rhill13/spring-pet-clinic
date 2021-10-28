b3edba547cb603b104738470fcbc8df190459d9a
Ryan <ryanhill1128@gmail.com>
2021-10-28 14:07:46 -0500

    added owner index page and controller, closes #13

---
+++ pet-clinic-web/src/main/java/biz/rookware/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package biz.rookware.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/owner", "/owner/index", "/owner/index.html"})
+public class OwnerController {
+    
+    @GetMapping
+    public String listOwners() {
+        return "owner/index";
+    }
+}
