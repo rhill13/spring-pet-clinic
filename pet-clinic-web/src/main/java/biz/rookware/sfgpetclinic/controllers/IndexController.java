c56def32e9c81f68dc8f8467521693b2ab30889a
Ryan <ryanhill1128@gmail.com>
2021-10-28 13:08:18 -0500

    added index page and controller, closes #8

---
+++ pet-clinic-web/src/main/java/biz/rookware/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package biz.rookware.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"","/", "index", "index.html"})
+public class IndexController {
+    
+    @GetMapping
+    public String index(Model model) {
+        return "index";
+    }
+}
