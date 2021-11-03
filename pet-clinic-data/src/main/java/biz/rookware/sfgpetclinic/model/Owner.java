4db9ab91cadc48f22ad4927675cdf1576e84e554
Ryan Hill <ryanhill1128@gmail.com>
2021-11-03 11:15:09 -0500

    added contact info to Owner, closes #28

--- pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/model/Owner.java
@@ -5,14 +5,35 @@ import java.util.Set;
 public class Owner extends Person {
     
     // Properties
+    private String address;
+    private String city;
+    private String phone;
     private Set<Pet> pets;
 
     // Getters and Setters
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
     public Set<Pet> getPets() {
         return pets;
     }
-
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+    
 }
