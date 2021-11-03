392b8c22750b277bbc8b03532e940f166f9706e3
Ryan Hill <ryanhill1128@gmail.com>
2021-11-03 12:43:44 -0500

    refactored to iterate over id's on save

--- pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,11 +4,19 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import biz.rookware.sfgpetclinic.model.Specialty;
 import biz.rookware.sfgpetclinic.model.Vet;
+import biz.rookware.sfgpetclinic.services.SpecialtyService;
 import biz.rookware.sfgpetclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public void delete(Vet object) {
@@ -32,6 +40,16 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        if (object.getSpecialties().size() > 0) {
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
     
