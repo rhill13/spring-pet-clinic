9d56908891752202fa48650e64d6c23b656c386a
Ryan Hill <ryanhill1128@gmail.com>
2021-11-03 11:12:35 -0500

    added Specialty model, added to Vet, closes #27

---
+++ pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,17 @@
+package biz.rookware.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity {
+    
+    // Properties
+    private String description;
+
+    // Getters and Setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
--- pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/biz/rookware/sfgpetclinic/model/Vet.java
@@ -1,5 +1,19 @@
 package biz.rookware.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    
+    // Properties
+    private Set<Specialty> specialties;
+
+    // Getters and Setters
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
     
 }
